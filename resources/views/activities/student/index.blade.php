2d8b82147e959e087871dc686fd8387a4c7a928a
clemckr <clemckr@gmail.com>
2018-04-12 16:59:48 +0200

    activite front

--- resources/views/activities/student/index.blade.php
+++ resources/views/activities/student/index.blade.php
@@ -1,4 +1,31 @@
-@extends ('partials.layout', ['title' => 'Boite à idées'])
-@section('content')
-accueil activités
+@extends ('partials.layout', ['title' => 'Activités à venir'])
+
+@section ('content')
+<div class="container">
+{{-- @if (1) (!$ideas->isEmpty()) [MEMBRE DU BDE] --}}
+    <?php $i = 0; ?>
+    <div style="overflow-x: auto;" id="test">
+        <table>
+            <tr>
+                <th>Activité</th>
+                <th>Date</th>
+                <th>Description</th>
+                <th>Lieu</th>
+                <th>S'inscrire</th>
+            </tr>
+        
+            <tr id="activity-1">
+                <td id="activity-name-1">Foot</td>
+                <td id="activity-date-1">{{ date('d-m-Y') }}</td>
+                <td id="activity-desc-1">Qui met des petits ponts à Amine</td>
+                <td id="activity-place-1">Strasbourg</td>
+                <td><button class="btn accept__btn" id="btnAccept-1" onclick="accept()">S'INSCRIRE</button></td>
+            </tr>
+            <?php $i++ ?>
+        </table>
+    </div>
+{{-- @else
+    <p>Aucune activité à venir</p>
+@endif --}}
+</div>
 @endsection
